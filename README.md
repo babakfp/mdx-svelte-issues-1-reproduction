02ff457b32528fd4f7cb794bca98b6882998a011
babakfp <babak.bxf@gmail.com>
2024-07-21 22:39:22 +0330

    .

--- README.md
+++
@@ -1,5 +0,0 @@
-# BabakFP.IR
-
-## Resources
-
--   I used https://shots.so to create the frames for the front-end portfolio posters.
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import "$lib/app.pcss"
     import Layout from "$lib/markdown/layouts/Layout.svelte"
 </script>
 
